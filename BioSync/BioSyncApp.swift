05e2d27b90f50ec46ae3364fac45a7d34b0ee10d
viveksingh004 <vivek.singh@thinkitive.com>
2023-09-14 14:48:11 +0530

    Initial commit

---
+++ BioSync/BioSyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  BioSyncApp.swift
+//  BioSync
+//
+//  Created by Thinkitive on 14/09/23.
+//
+
+import SwiftUI
+
+@main
+struct BioSyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
